ab3a379e7cdbe6147a8307fb2bbcc2441b82fff1
Maria <maria.lazareva.ds@gmail.com>
2021-11-27 20:20:57 +0300

    6 files

--- Homework1/task3.py
+++
@@ -1,8 +0,0 @@
-# 3. Узнайте у пользователя число n. Найдите сумму чисел n + nn + nnn.
-# Например, пользователь ввёл число 3. Считаем 3 + 33 + 333 = 369.
-
-string_1 = input('Введите цифру от 1 до 9: ')
-string_2 = string_1 * 2
-string_3 = string_1 * 3
-result = int(string_1) + int(string_2) + int(string_3)
-print('Результат: ' + str(result))
